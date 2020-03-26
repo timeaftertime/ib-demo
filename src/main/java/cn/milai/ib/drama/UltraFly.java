d77afc7bfe07b0a307406477b4e4d6974c0fb667
timeaftertime <919379706@qq.com>
2020-03-26 18:18:47 +0800

    完成默认剧情的关卡一（待重构）

---
+++ src/main/java/cn/milai/ib/drama/UltraFly.java
@@ -0,0 +1,12 @@
+package cn.milai.ib.drama;
+
+import cn.milai.ib.character.AbstractIBCharacter;
+import cn.milai.ib.container.Container;
+
+public class UltraFly extends AbstractIBCharacter {
+
+	public UltraFly(int x, int y, Container container) {
+		super(x, y, container);
+	}
+
+}
